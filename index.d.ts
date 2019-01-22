46a2c57d1a421bf5aa4596fc5b9980ed28250bc7
Alex Lunyov <isuntc@gmail.com>
2019-01-22 14:45:12 +0800

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare module 'nano-tween' {
+  type TweenParams = {
+    duration?: number;
+    easing?: (time: number) => number;
+    fps?: number;
+  };
+
+  function tween(
+    from: number,
+    to: number,
+    cb: (value: number) => void,
+    params: TweenParams,
+  );
+  module.exports = tween;
+}
---
+++ src/index.js
@@ -0,0 +1,28 @@
+var runWithFps = require('run-with-fps');
+
+function linear(a) {
+  return a;
+}
+
+function tween(from, to, cb, params) {
+  var duration = params.duration || 300;
+  var easing = params.easing || linear;
+  var fps = params.fps || 60;
+
+  var stopped = false;
+  var start = Date.now();
+
+  var stop = runWithFps(function() {
+    var spent = Math.min(Date.now() - start, duration);
+    cb(from + (to - from) * easing(spent / duration));
+    if (spent === duration || stopped) {
+      stop();
+    }
+  }, fps);
+
+  return function() {
+    stopped = true;
+  };
+}
+
+module.exports = tween;
